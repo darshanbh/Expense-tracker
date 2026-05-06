db939cd7b04d067a96650129dc5a38e6b35f4377
darshanbh <darshanbh8@gmail.com>
2026-05-07 01:01:20 +0530

    Final deployment update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expense-tracker-m41f.onrender.com",
+
 });
 
 API.interceptors.request.use((req) => {
